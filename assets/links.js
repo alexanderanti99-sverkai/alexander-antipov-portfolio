364ec32a84ca5f296a67bf2e0c6d92fda9a6f98d
alexanderanti99-sverkai <273800012+alexanderanti99-sverkai@users.noreply.github.com>
2026-05-20 16:05:15 +0300

    Fix portfolio demo link targets

--- assets/links.js
+++ assets/links.js
@@ -1,4 +1,4 @@
 ﻿window.PORTFOLIO_LINKS = {
-  sverkai: "https://8400698ac3b3df99-79-139-134-126.serveousercontent.com",
+  sverkai: "https://8400698ac3b3df99-79-139-134-126.serveousercontent.com/app",
   vkBeatDistributor: "https://26ae5e542418254a-79-139-134-126.serveousercontent.com"
 };
